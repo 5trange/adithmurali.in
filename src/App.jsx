91c89f7dbd94a4e95ced970c428459f9cbf0b3c1
5trange <MacbookAirM4>
2025-08-09 14:19:04 +0530

    Add details

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,81 @@ function App() {
     return (
         <>
             <h1>Adith K. Murali</h1>
+
             <p>
                 IT professional with more than 3 years of experience in
                 implementing and supporting Oracle B2C Service Cloud and Oracle
                 CRM solutions. Skilled in PHP, JavaScript, and CodeIgniter
                 Framework, with expertise in database management and building
-                efficient CRM solutions.
+                efficient CRM solutions. Enthusiastic learner, always learning
+                something new.
+            </p>
+
+            <ol class="twocol">
+                <li>
+                    <a href="https://github.com/5trange">GitHub Profile</a>
+                </li>
+                <li>
+                    <a href="mailto:adithmurali.gg@gmail.com">Email Me</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/adithmurali/">
+                        LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/5trange?tab=repositories">
+                        Projects
+                    </a>
+                </li>
+            </ol>
+
+            <h2 id="experience">Experience</h2>
+
+            <h3>Speridian Technologies(2022 - Present)</h3>
+            <p>
+                hands-on experience in working with a broad range of Oracle CRM
+                products including Oracle B2C Service Cloud, Oracle Field
+                Service Cloud, and Oracle Integration Cloud, I have successfully
+                developed, deployed, and supported CRM solutions that enhance
+                client relationships, optimize business processes, and ensure
+                seamless system performance.
+            </p>
+
+            <h2>Projects</h2>
+            <h3>Razer, Inc. Support Site (January 2023 - Present)</h3>
+            <p>
+                At Razer Inc., I worked on a range of client requirements, with
+                a primary focus on developing and enhancing Oracle B2C Service
+                Cloud. My responsibilities encompassed customer portal
+                development, BUI extensions, integrations, APIs, reporting, and
+                other critical components. Additionally, I contributed to the
+                implementation of AI/ML Accelerators within Oracle B2C Service
+                Cloud.
+            </p>
+            <h3>HomeHelpy - Primech Holdings (July 2022 - January 2023)</h3>
+            <p>
+                Developed and managed pages within the RightNow Customer Portal
+                using PHP and JavaScript. Contributed to various components,
+                including Oracle Field Service Cloud, BUI Extensions, and APIs,
+                among others.
+            </p>
+
+            <h3>Academic Project - Liquid Media Player</h3>
+            <p>
+                Lightweight media player written in C++ using FFmpeg and SDL2.
+                Fully opensource, licenced under GNU GPL v2. Get it{" "}
+                <a href="https://github.com/ArrowInteractive/liquid">here.</a>
+            </p>
+
+            <h3>Academic Project - ClamGuard Antivirus</h3>
+            <p>
+                Free and opensource antivirus based on ClamAV. Simple GUI with
+                functionalities including different types of scans, quarantine
+                system, etc. A background process of ClamGuard watches the
+                system drive for any malicious code. Fully opensource, licenced
+                under GNU GPL v2. Get it{" "}
+                <a href="https://github.com/5trange/ClamGuard">here.</a>
             </p>
         </>
     );
